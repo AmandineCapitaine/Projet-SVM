acb506ade3aa63ff3a8129f72c243aa3f2a77daa
AmandineCapitaine <81820916+AmandineCapitaine@users.noreply.github.com>
2021-05-24 20:52:52 +0200

    Add files via upload

---
+++ NN et Auto encodeur/class_ae.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            # 28 x 28
+            nn.Conv2d(1, 4, kernel_size=5),
+            # 4 x 24 x 24
+            nn.ReLU(True),
+            nn.Conv2d(4, 8, kernel_size=5),
+            nn.ReLU(True),
+            # 8 x 20 x 20 = 3200
+            nn.Flatten(),
+            nn.Linear(3200, 10),
+            # 10
+            nn.Softmax()
+        )
+        self.decoder = nn.Sequential(
+            # 10
+            nn.Linear(10, 400),
+            # 400
+            nn.ReLU(True),
+            nn.Linear(400, 4000),
+            # 4000
+            nn.ReLU(True),
+            nn.Unflatten(1, (10, 20, 20)),
+            # 10 x 20 x 20
+            nn.ConvTranspose2d(10, 10, kernel_size=5),
+            # 24 x 24
+            nn.ConvTranspose2d(10, 1, kernel_size=5),
+            # 28 x 28
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+    
+    def encodeur_only(self, x):
+        return self.encoder(x)
